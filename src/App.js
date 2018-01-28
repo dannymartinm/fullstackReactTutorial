d72472565736303b86d9496a8749881c304b8948
Daniela Martin <danielamartinmtz@gmail.com>
2018-01-27 22:08:42 -0600

    Se dividieron los componentes. Título del header es el que le paso en el App

--- src/App.js
+++ src/App.js
@@ -11,7 +11,54 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Timeline />
+        <div className="panel">
+          <Header title="hola" />
+          <Content />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="menuIcon">
+          <div className="dashTop" />
+          <div className="dashBottom" />
+          <div className="circle" />
+        </div>
+        <span className="title">{this.props.title}</span>
+        <input type="text" className="searchInput" placeholder="Search..." />
+        <div className="fa fa-search searchIcon" />
+      </div>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="content">
+        <div className="line" />
+
+        {/* Timeline item */}
+        <div className="item">
+          <div className="avatar">
+            <img
+              alt="Doug"
+              src="http://www.croop.cl/UI/twitter/images/doug.jpg"
+            />
+            Doug
+          </div>
+
+          <span className="time">An hour ago</span>
+          <p>Ate lunch</p>
+          <div className="commentCount">2</div>
+        </div>
+
+        {/* ... */}
       </div>
     );
   }
