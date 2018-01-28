91687539f9f0436cda0f89ad49d25ae8a24cdabd
Daniela Martin <danielamartinmtz@gmail.com>
2018-01-27 21:18:48 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./Timeline.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Timeline />
+      </div>
+    );
+  }
+}
+
+class Timeline extends React.Component {
+  render() {
+    return (
+      <div className="notificationsFrame">
+        <div className="panel">
+          <div className="header">
+            <div className="menuIcon">
+              <div className="dashTop" />
+              <div className="dashBottom" />
+              <div className="circle" />
+            </div>
+
+            <span className="title">Timeline</span>
+
+            <input
+              type="text"
+              className="searchInput"
+              placeholder="Search ..."
+            />
+
+            <div className="fa fa-search searchIcon" />
+          </div>
+          <div className="content">
+            <div className="line" />
+            <div className="item">
+              <div className="avatar">
+                <img
+                  alt="doug"
+                  src="http://www.croop.cl/UI/twitter/images/doug.jpg"
+                />
+              </div>
+
+              <span className="time">An hour ago</span>
+              <p>Ate lunch</p>
+            </div>
+
+            <div className="item">
+              <div className="avatar">
+                <img
+                  alt="doug"
+                  src="http://www.croop.cl/UI/twitter/images/doug.jpg"
+                />
+              </div>
+
+              <span className="time">10 am</span>
+              <p>Read Day two article</p>
+            </div>
+
+            <div className="item">
+              <div className="avatar">
+                <img
+                  alt="doug"
+                  src="http://www.croop.cl/UI/twitter/images/doug.jpg"
+                />
+              </div>
+
+              <span className="time">10 am</span>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </p>
+            </div>
+
+            <div className="item">
+              <div className="avatar">
+                <img
+                  alt="doug"
+                  src="http://www.croop.cl/UI/twitter/images/doug.jpg"
+                />
+              </div>
+
+              <span className="time">2:21 pm</span>
+              <p>
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
